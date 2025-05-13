59aebefa6d2c656a5cc6d427b5ed9ccf2a4b74a2
Hitesh918 <118730681+Hitesh918@users.noreply.github.com>
2025-05-13 22:07:46 +0530

    done

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5000"; 
-// const BASE_URL = "https://reaserver.onrender.com"; 
+// const BASE_URL = "http://localhost:5000"; 
+const BASE_URL = "https://reaserver.onrender.com"; 
 
 export default BASE_URL;
